d8a19140924290fb89378a8c462cb29b2613b21d
leozinga <zleho70@gmail.com>
2023-04-21 03:29:19 -0300

    added readme

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,19 +1,25 @@
 const sliderButton = document.querySelector('.slider-button')
 const sliderItems = document.querySelectorAll('.gallery-slider-img')
-let clicked = false
+let counter = 1
 
 sliderButton.addEventListener("click", () => {
-    if(clicked === false) {
+    if(counter === 1) {
         for(item of sliderItems) {
             item.style.right = "100%"
+            sliderButton.style.rotate = "90deg"
+        }
+        counter++
+    } else if(counter === 2){
+        for(item of sliderItems) {
+            item.style.right = "200%"
             sliderButton.style.rotate = "180deg"
         }
-        clicked = true
-    } else {
+        counter++
+    } else if(counter === 3) {
         for(item of sliderItems) {
             item.style.right = "0"
             sliderButton.style.rotate = "0deg"
-            clicked = false
         }
+        counter = 1
     }
 })
